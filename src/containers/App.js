fd84e50a540d0005d65b5cd95f9bb0dde4f5f534
mkdir-helen <experiment.helen@gmail.com>
2019-02-28 14:50:55 -0500

    connect to app.css

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
+import './App.css';
 
 class App extends Component {
     constructor() {
